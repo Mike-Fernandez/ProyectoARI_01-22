6fe2cafa3c107ab5e8503550dc1c3255d79ce726
Mike-Fernandez <00145518@uca.edu.sv>
2022-06-17 18:54:45 -0600

    Convert to JSON funciona

--- Proyecto.py
+++ Proyecto.py
@@ -8,8 +8,16 @@ root.title("Testing")
 root.geometry("200x200")
 
 
-#textBox = Entry(root)
-#textBox.pack()
+def convertRowtoJSON(row):
+    return """\t{
+        \t\"documento\" : \"%s\",
+        \t\"primer-nombre\" : \"%s\",
+        \t\"apellido\" : \"%s\",
+        \t\"credit-card\" : \"%s\",
+        \t\"tipo\" : \"%s\",
+        \t\"telefono\" : \"%s\"
+\t}""" %(row[0],row[1],row[2],row[3],row[4],row[5])
+
 
 def convertRowtoXML(row):
     return """\t<cliente>
@@ -41,11 +49,39 @@ def toXML():
 
     print("it worked!")
 
-    # TODO: convertir csv to xml
+def toJSON():
+    root.filename = filedialog.askopenfilename(initialdir="/Users/operator/Documents/ARI/Proyecto", title="Select a file", filetypes=(("csv files", "*.csv"),("all files", "*.*")))
+    
+    f = open(root.filename)
+    csv_f = csv.reader(f, delimiter=';')
+    data = []
+
+    for row in csv_f:
+        data.append(row)
+    f.close()
+
+    print(data)
+
+    with open('output.json', 'w') as w:
+        w.write("[\n")
+        w.write('\n'.join([(convertRowtoJSON(n) + ',') for n in data]))
+    
+    with open('output.json', 'r') as r:
+        fix = r.read()[:-1]
+    with open('output.json', 'w') as w:
+        w.write(fix)
+        w.write("\n]")
+
+    print("it worked!")
+
 
 label1 = Label(root, text="Seleccione el archivo para convertirlo a XML").pack()
 toXMLbutton = Button(root, text="Convertir a XML", command=toXML)
 toXMLbutton.pack()
+
+label2 = Label(root, text="Seleccione el archivo para convertirlo a JSON").pack()
+toJSONbutton = Button(root, text="Convertir a JSON", command=toJSON)
+toJSONbutton.pack()
 
 buttonQuit = Button(root, text="Exit", command=root.quit)
 buttonQuit.pack()
